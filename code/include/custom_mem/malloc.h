b64000208a2f042e2f73aedcd260b8b9e7bb9b7d
Dennis Yakovlev <dennis.yakovlev@yahoo.ca>
2022-09-25 00:32:51 -0400

    fixed, added docs

--- code/include/custom_mem/malloc.h
+++ code/include/custom_mem/malloc.h
@@ -1,32 +1,20 @@
 #pragma once
 #include <stddef.h>
 
-/*  Extra features
-        1) fragmentation clean up
-                each time malloc or free is called, some fragmentation cleanup happens
-        2) hints
-                keep hints, speeds up allocation but takes more memory
-                - maybe like array of pointers to open spaces for different sizes
-*/
-
-/*  To improve efficient, can have a ds in MallocInfo which contains pointers
-    to all our blocks, then we can allocate based on that, and rearrange
-    the pointer to next inside the blocks as necessary when modifying the ds.
-*/
-
-// have a fast cache for allocation less than X bytes, and to find which is free
-// use what we did with old malloc and bits to represent which are and arent taken
-//  use asm instruction to find first 1, which represent free?
-
 struct MallocAdjustables
 {
-    // minimum amount of more memory to request at a time
+    /* Minimum amount of more memory to request at a time.
+    */
     size_t more_mem;
 
-    // 
+    /* Fast cache size.
+    */
     size_t cache_sz;
 };
 
+// request n bytes of contiguous memory
 void* my_malloc(size_t);
 
+// free memory previously requested with
+// my_malloc
 void my_free(void*);
--- code/source/malloc.c
+++ code/source/malloc.c
@@ -1,24 +1,59 @@
-/*  Organize the heap into linked list of mmap'ed mappings. Each mapping has a
-    linked list of blocks. Each block is a linked list of allocations.
+/*  General idea is to organize the heap into conceptual
+    linked lists. Where each layer of linked list is an array
+    which is iterated through using the size of nodes.
 
-    An allocation causes a searching all three levels of linked lists. A first fit
-    strategy is used for allocations. Once a space is found in a block, the space
-    gets set to taken and the entire block is interated over to update meta data.
+    There are three layers of linked lists
+        - Mapping
+        - Block
+        - Allocations
+    
+    Overhead:
 
-    Freeing memory sets the previously allocated space to free, without updating
-    the block metadata. The metadata can only be updated when another allocation
-    is put in the block.
+        There is overhead at every level of linked list.
+        The amount of overhead for Mapping's and Block's
+        is neglegable compared to the number of
+        allocated bytes.
+        Every allocation has MY_MALLOC_ALLOC_META bytes
+        of overhead. Usually 8 amd 16 bytes for 32 and
+        64 bit systems respectively.
+
+    Allocation:
+
+        An allocation causes a searching all three levels
+        of linked lists. 
+        A first fit strategy is used for allocations.
+        Once a space is found in a block, the space
+        gets set to taken and the entire block is iterated
+        over to update meta data.
+    
+    Free:
+
+        A free causes a looping over of the block the
+        memory was requested from.
+
+    Constraints:
+
+        - Can allocate at most size_t minus 1 bitwidth
+          in one allocation.
+
+    Notes:
+
+        - Returning a void pointer from "my_malloc" then
+          having a void pointer as the param to "my_free"
+          is undefined behaviour. Since 
+            some_ptr -> void* -> some_ptr
+          is defined. But
+            void* -> some_ptr -> void*
+          is not defined.
+
+          The second set of conversions is avoided elsewhere.
 */
 
 #include <custom_mem/malloc.h>
 #include <stdatomic.h>
 #include <assert.h>
 #include <sys/mman.h> // mmap
-#include <unistd.h>   // sbrk, brk
 #include <stdint.h>   // SIZE_MAX
-
-// for large allocations, instead of using below bit field macros, use two variables
-// like in block to be able to hold entire size
 
 typedef struct MallocGlobal
 {
@@ -26,40 +61,106 @@ typedef struct MallocGlobal
 }
 _global;
 
+/*  Is the top level in linked list chain.
+    Mapping's cannot be locked. They consist
+    of blocks.
+
+    Every mapping is requested with one
+    mmap call.
+*/
 typedef struct MallocMapping
 {
-    // starting block in this mapping
+    /* Starting block in this mapping.
+    */
     void* start_block;
 
-    // ending block in this mapping
+    /* Ending block in this mapping.
+    */
     void* end_block;
 
-    // next mapping
+    /* Next mapping.
+    */
     struct MallocMapping* next;
 
-    // start of this mapping
+    /* Start of this mapping.
+    */
     void* start;
 
-    // end of this mapping
+    /* End of this mapping.
+    */
     void* end;
 }
 _mapping;
 
+/*  Is the second level in linked list chain.
+    Blocks can be locked. They consist of 
+    allocations.
+
+    At any time in the block there is always
+    atleast one allocation meta data which
+    can be used for an allocation.
+    The gaurenteed meta data will be at the
+    end of the block. However, there may
+    be other free spaces within the block.
+        ie
+        BLOCK_META_DATA         |
+        META_DATA (sz_1,used)   |
+        ...                     | 
+            in use memory       |
+        ...                     |
+        META_DATA (sz_2,free) <- max_free_ptr
+        ...             |       |
+            free memory | max_free
+        ...             |       |
+        META_DATA (sz_3,used)   |
+        ...                     |
+            in use memory       |
+        ...                     | sz
+        META_DATA (sz_4,free)   |
+        ...                     |
+            free memory         |
+        ...                     |
+        END OF BLOCK <- next
+
+
+        The last meta data before end of block
+        will always be there.
+
+    Note: For simplicity, the above block is
+          respresented as
+        
+        (sz_1,used) -> (sz_2,free) -> (sz_3,used) -> (sz_4,free) -> 
+        
+        Where sz_N represents the amount of bytes that can be
+        allocated in a particular node. 
+*/
 typedef struct MallocBlock
 {
-    // maximum contiguous number of free allocatable bytes
+    /*  Maximum contiguous number of free contiguous bytes.
+    */
     size_t max_free;
 
-    // size in bytes of storage this block takes
+    /*  Size in bytes this block takes in its entirety.
+    */
     size_t sz;
 
-    // whether being modified currently
+    /*  Whether being modified currently.
+    */
     volatile atomic_char is_free;
 
-    // next block
+    /*  Next block.
+
+        The next block will be directly after the
+        current block in memory.
+        If no next block, then NULL.
+
+        Keep this here to allow for easy future
+        changes.
+    */
     void* next;
 
-    // pointer to metadata of where max free space is
+    /*  Pointer to metadata of where max free space is.
+    */
     void* max_free_ptr;
 }
 _block;
@@ -87,60 +188,81 @@ typedef struct MallocAdjustables _vars;
     static_assert(0, "not gcc or clang")
 #endif
 
-// meta data is 
-// size_t - size of this allocation
-// +
-// void* - start of block meta data
+/*  How many bytes should the block take.
 
-// how many bytes do i want my block to take
+    This is needed because for a requested number of
+    bytes, there is some extra number of bytes needed
+    for meta data.
+    
+    {    1    }   {     2      }   {        3         }
+    (sz | 1024) + sizeof(_block) + MY_MALLOC_ALLOC_META
+
+    1 - padding room so that every allocation doesn't
+        require a new block
+    2 - block meta data
+    3 - atleast one metadata will be needed for
+        atleast one allocation
+*/
 #define MY_MALLOC_BLOCK_EXPANSION(sz) \
     (((sz) | 1024) + sizeof(_block) + MY_MALLOC_ALLOC_META)
 
-/* meta data per allocation
+/*  Meta data per allocation.
 
-   meta data is 
-   size_t - size of this allocation
-   +
-   void* - start of block meta data which
-           allocation is in
+    meta data is 
+    size_t - size of this allocation
+    +
+    void* - start of block meta data which
+            allocation is in
 */
 #define MY_MALLOC_ALLOC_META \
     (sizeof(size_t) + sizeof(void*))
 
+/*  Get pointer to availability.
+*/
+#define MY_MALLOC_GET_AVAILABILITY_PTR(VP_META) \
+    ((char*)(VP_META) + sizeof(size_t))
+
 // get size of allocation
+// should not include meta data
 #define MY_MALLOC_GET_SIZE(VP_META) \
     (*(size_t*)(VP_META))
 
-// get whether allocation is free
-// NULL -> free
-// else -> not free, start of block
+/*  Get whether allocation is free.
+    
+    NULL -> free
+    else -> not free, start of block
+*/
 #define MY_MALLOC_GET_AVAILABILITY(VP_META) \
-    ((char*)(VP_META) + sizeof(size_t))
+    *(void**)MY_MALLOC_GET_AVAILABILITY_PTR(VP_META)
 
-// set size of allocation
+/* Set size of allocation.
+*/
 #define MY_MALLOC_SET_SIZE(VP_META, SZ) \
     *(size_t*)(VP_META) = SZ
 
-// set allocation to free
+/*  Set allocation to free.
+*/
 #define MY_MALLOC_SET_FREE(VP_META) \
     do \
     { \
-        void* temp = MY_MALLOC_GET_AVAILABILITY(VP_META); \
-        temp = NULL; \
+        void* temp = MY_MALLOC_GET_AVAILABILITY_PTR(VP_META); \
+        *(void**)temp = NULL; \
         if (temp); \
     } while (0)
     
-
-// set allocation to inuse
+/* Set allocation to inuse.
+*/
 #define MY_MALLOC_SET_INUSE(VP_META, VP_BLOCK) \
     do \
     { \
-        void* temp = MY_MALLOC_GET_AVAILABILITY(VP_META); \
-        temp = VP_BLOCK; \
+        void* temp = MY_MALLOC_GET_AVAILABILITY_PTR(VP_META); \
+        *(void**)temp = VP_BLOCK; \
         if (temp); \
     } while (0)
 
-// move to start of meta data for next allocation
+/*  Iterate a meta data pointer forward to the
+    next possible meta data.
+*/
 #define MY_MALLOC_NEXT(VP_META) \
     ((char*)(VP_META) + MY_MALLOC_ALLOC_META + MY_MALLOC_GET_SIZE(VP_META))
 
@@ -154,34 +276,6 @@ static _vars G_vars =
     .more_mem = 1048576,
     .cache_sz = 64
 };
-
-static _lock G_lock =
-{
-    .state = 1
-};
-
-// dont wait inside the lock, just say you failed to get
-
-int    _global_lock_get()
-{
-    char expected = 1;
-    if (atomic_compare_exchange_strong(&G_lock.state, &expected, 0))
-    {
-        return 0;
-    }
-
-    // 1) will probably get more memory before swapped
-    // 2) will only wait for couple microseconds, can
-    //    peg the cpu without "pause" instructions
-    while (!G_lock.state);
-
-    return G_lock.state;
-}
-
-void   _global_lock_free()
-{
-    atomic_store(&G_lock.state, 1);
-}
 
 void*  _mem_get(size_t bytes)
 {
@@ -217,10 +311,12 @@ size_t _mem_more_sz(size_t bytes)
 int    _block_has_room(size_t bytes, _block* block)
 {
     // whether block has enough room for bytes
+    // plus an allocation meta data
     // return 1 is yes, 0 otherwise
 
     const size_t free = block->max_free;
-    if (free <= MY_MALLOC_ALLOC_META) {
+    if (free <= MY_MALLOC_ALLOC_META)
+    {
         return 0;
     }
 
@@ -229,24 +325,146 @@ int    _block_has_room(size_t bytes, _block* block)
 
 void   _block_update_meta(void* block)
 {
-    // set new largest allocatable size for block
+    // update the block meta data to reflect
+    // information in the allocation meta data's
+
+    /*  Algorithm explained.
+
+        Instead of using this representation
+            (sz_1,used) -> (sz_2,free) -> (sz_3,used) ->
+        Use the following, which only shows whether
+        a node is U(sed) or F(ree)
+            U -> F -> U ->
+
+
+
+        This function is called after at most one
+        manipulation has been done to a block. Either
+        a free or allocation.The point is to
+        correctly update block and allocation meta
+        data to a state such that the block is valid.
+
+        A valid block meets all the conditions
+            - block meta data is correct
+            - at most one free consecutive node
+
+        To update first, loop through all nodes keeping
+        track of the largest free node. Updating meta
+        data at the end.
+
+        Second condition is harder.
+            Consider the following list
+                U -> F -> U -> U ->
+            which, with a free, can be turned into
+                U -> F -> F -> U ->
+            
+            Consider as well
+                U -> F -> U -> F ->
+            which, with a free, can be turned into
+                U -> F -> F -> F ->
+
+            Consider
+                U -> F -> U ->
+            which, with an allocation could be
+                U -> U -> U ->
+
+            With any single free, atmost three
+            consecutive frees will occur. Need to
+            merge [0,3] consecutive frees.
+    */
+
     _block* block_ptr = block;
 
-    void* curr = (char*)block + sizeof(_block), *max = curr;
-    size_t i = 0;
+    size_t i = sizeof(_block);
+    void* prev = (char*)block + sizeof(_block);
+
+    while (i < block_ptr->sz && MY_MALLOC_GET_AVAILABILITY(prev))
+    {
+        i += MY_MALLOC_GET_SIZE(prev) + MY_MALLOC_ALLOC_META;
+        prev = MY_MALLOC_NEXT(prev);
+    }
+
+    if (i >= block_ptr->sz)
+    {
+        // didn't find a free node
+
+        block_ptr->max_free = 0;
+        block_ptr->max_free_ptr = NULL;
+
+        return;
+    }
+
+    void* max = prev, *curr = MY_MALLOC_NEXT(prev);
+    i += MY_MALLOC_GET_SIZE(prev) + MY_MALLOC_ALLOC_META;
+
+    /*  If execution reaches here prev will be located at
+        the first free allocation's meta data.
+        curr (i) will be the possible node after prev.
+
+        A possible state at this point is
+
+        (taken,sz_1) -> (taken,sz_2) -> (free,sz_3) -> (taken/free,sz_4) -> ...
+                                        prev           curr (i)
+        
+        i = sz_1 + sz_2 + sz_3 + (META * 3) + BLOCK_META
+    */
+
     while (i < block_ptr->sz)
     {
-        size_t curr_size = MY_MALLOC_GET_SIZE(curr);
-        if (MY_MALLOC_GET_AVAILABILITY(curr) && curr_size > MY_MALLOC_GET_SIZE(max))
+        size_t curr_sz = MY_MALLOC_GET_SIZE(curr);
+
+        // merge previous and curr if both free
+        if
+        (
+            prev
+            &&
+            !MY_MALLOC_GET_AVAILABILITY(prev)
+            &&
+            !MY_MALLOC_GET_AVAILABILITY(curr)
+        )
+        {
+            // prev and curr are both free, merge
+
+            size_t old_curr_sz = curr_sz;
+
+            curr_sz += MY_MALLOC_GET_SIZE(prev) + MY_MALLOC_ALLOC_META;
+
+            void* next = MY_MALLOC_NEXT(curr);
+            if 
+            (
+                i + MY_MALLOC_GET_SIZE(curr) + MY_MALLOC_ALLOC_META < block_ptr->sz
+                &&
+                !MY_MALLOC_GET_AVAILABILITY(next)
+            )
+            {
+                // next is also free, merge all three
+
+                curr_sz += MY_MALLOC_GET_SIZE(next) + MY_MALLOC_ALLOC_META; 
+            }
+
+            MY_MALLOC_SET_SIZE(prev, curr_sz);
+
+            curr = prev;
+            prev = NULL;
+
+            curr_sz = old_curr_sz;
+        }
+
+        if
+        (
+            !MY_MALLOC_GET_AVAILABILITY(curr)
+            &&
+            MY_MALLOC_GET_SIZE(curr) > MY_MALLOC_GET_SIZE(max)
+        )
         {
             max = curr;
         }
 
-        i += curr_size + MY_MALLOC_ALLOC_META;
-        curr = MY_MALLOC_NEXT(curr);
+        i += curr_sz + MY_MALLOC_ALLOC_META;
     }
-    block_ptr->max_free_ptr = max;
+
     block_ptr->max_free = MY_MALLOC_GET_SIZE(max);
+    block_ptr->max_free_ptr = max;
 }
 
 void*  _block_alloc_unsafe(size_t bytes, void* block)
@@ -255,34 +473,36 @@ void*  _block_alloc_unsafe(size_t bytes, void* block)
     // allocation in block
     // return the start of allocation
 
-    // Assume: bytes < block.max_free - ALLOC_META
+    // Assume: bytes <= block.max_free - ALLOC_META
 
     _block* block_ptr = block;
 
-    void* to_ret = (char*)block_ptr->max_free_ptr + MY_MALLOC_ALLOC_META;
+    void* alloc_start = (char*)block_ptr->max_free_ptr + MY_MALLOC_ALLOC_META;
 
-    MY_MALLOC_SET_INUSE(block_ptr->max_free_ptr, block);
+    /*  Always add another allocation meta data.
+
+        For exmaple
+        (1500,used) -> (1000,free) -> (2000,used) -> ...
+        Can turn into
+        (1500,used) -> (984,used) -> (0,free) -> (2000,used) -> ...
+
+        Point is, an allocation meta data will
+        always be added to maintain structure. Even
+        at the cost of wasted space.
+    */
     size_t remaining = block_ptr->max_free - bytes - MY_MALLOC_ALLOC_META;
 
+    MY_MALLOC_SET_INUSE(block_ptr->max_free_ptr, block);
     MY_MALLOC_SET_SIZE(block_ptr->max_free_ptr, bytes);
 
-    if (remaining <= G_vars.cache_sz) // should be greater than fast cache
-    {
-        // no more room in block
-        block_ptr->max_free = 0;
-        block_ptr->max_free_ptr = NULL;
-
-        return to_ret;
-    }
-
-    // add another space for potential allocation
-    void* after_insert = (char*)block_ptr->max_free_ptr + bytes + MY_MALLOC_ALLOC_META;
+    // set up meta data for another allocation
+    void* after_insert = MY_MALLOC_NEXT(block_ptr->max_free_ptr);
     MY_MALLOC_SET_FREE(after_insert);
     MY_MALLOC_SET_SIZE(after_insert, remaining);
 
     _block_update_meta(block);
 
-    return to_ret;
+    return alloc_start;
 }
 
 void   _block_create_unsafe(size_t sz, void* where)
@@ -290,15 +510,28 @@ void   _block_create_unsafe(size_t sz, void* where)
     // create a block of sz starting the block on where
     // return the total number of bytes taken
 
+    _block* block_ptr = (_block*)where;
+
+    /*  Create the block with a meta data subtracted since
+        initialzation requires two meta data's, but all
+        other cases require one.
+    */
     _block new_block =
     {
         .sz           = sz,
         .is_free      = 1,
         .next         = NULL,
         .max_free_ptr = (char*)where + sizeof(_block),
-        .max_free     = sz - sizeof(_block)
+        .max_free     = sz - sizeof(_block) - MY_MALLOC_ALLOC_META
     };
-    *(_block*)where = new_block;
+    
+    *block_ptr = new_block;
+
+    void* initial_alloc = (char*)where + sizeof(_block); 
+    MY_MALLOC_SET_FREE(initial_alloc);
+    MY_MALLOC_SET_SIZE(initial_alloc, block_ptr->max_free);
+    // MY_MALLOC_SET_SIZE(initial_alloc, block_ptr->sz);
+    // used to be this... ???
 }
 
 void*  _block_get(size_t bytes, _mapping** mapping)
@@ -391,7 +624,7 @@ void*  _mapping_block_create(size_t bytes, _mapping** mapping)
     // return start of allocation
 
     _block* end_block = (*mapping)->end_block;
-    void* where = (char*)(*mapping)->end_block + sizeof(_block) + end_block->sz;
+    void* where = (char*)(*mapping)->end_block + end_block->sz;
     size_t block_sz = MY_MALLOC_BLOCK_EXPANSION(bytes);
 
     if (block_sz > (char*)(*mapping)->end - (char*)where)
@@ -430,9 +663,11 @@ void*  my_malloc(size_t bytes)
     return _mapping_block_create(bytes, &mapping);
 }
 
-void my_free(void* ptr)
+void   my_free(void* ptr)
 {
-    // free usage in block
-    // get pointer to start of block
-    // call _block_update_meta
+    // set an allocation to be freed
+
+    void* block = MY_MALLOC_GET_AVAILABILITY((char*)ptr - MY_MALLOC_ALLOC_META);
+    MY_MALLOC_SET_FREE((char*)ptr - MY_MALLOC_ALLOC_META);
+    _block_update_meta(block);
 }
